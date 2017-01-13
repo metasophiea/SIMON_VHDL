c8e80ae55fb90db32bd8b999f0b90b9ab80e1cc4
metasophiea <metasophiea@gmail.com>
2017-01-13 20:06:44 +0000

    20170113-a - 1.0.0 - first working version

--- 1.0.0/build_c.sh
+++ 1.0.0/build_c.sh
@@ -9,6 +9,14 @@ ghdl -s messageEncrypter.hdl
 ghdl -s messageDecrypter.hdl
 ghdl -s simon_32_64.hdl
 ghdl -s simon_48_72.hdl
+ghdl -s simon_48_96.hdl
+ghdl -s simon_64_96.hdl
+ghdl -s simon_64_128.hdl
+ghdl -s simon_96_96.hdl
+ghdl -s simon_96_144.hdl
+ghdl -s simon_128_128.hdl
+ghdl -s simon_128_192.hdl
+ghdl -s simon_128_256.hdl
 ghdl -s main.hdl
 ghdl -s tester.hdl
 
@@ -18,6 +26,14 @@ ghdl -a messageEncrypter.hdl
 ghdl -a messageDecrypter.hdl
 ghdl -a simon_32_64.hdl
 ghdl -a simon_48_72.hdl
+ghdl -a simon_48_96.hdl
+ghdl -a simon_64_96.hdl
+ghdl -a simon_64_128.hdl
+ghdl -a simon_96_96.hdl
+ghdl -a simon_96_144.hdl
+ghdl -a simon_128_128.hdl
+ghdl -a simon_128_192.hdl
+ghdl -a simon_128_256.hdl
 ghdl -a main.hdl
 ghdl -a tester.hdl
 
--- 1.0.0/build_s.sh
+++ 1.0.0/build_s.sh
@@ -9,5 +9,13 @@ ghdl -s messageEncrypter.hdl
 ghdl -s messageDecrypter.hdl
 ghdl -s simon_32_64.hdl
 ghdl -s simon_48_72.hdl
+ghdl -s simon_48_96.hdl
+ghdl -s simon_64_96.hdl
+ghdl -s simon_64_128.hdl
+ghdl -s simon_96_96.hdl
+ghdl -s simon_96_144.hdl
+ghdl -s simon_128_128.hdl
+ghdl -s simon_128_192.hdl
+ghdl -s simon_128_256.hdl
 ghdl -s main.hdl
 ghdl -s tester.hdl
