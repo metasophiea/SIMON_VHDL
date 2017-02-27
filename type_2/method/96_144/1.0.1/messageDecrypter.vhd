571856a575d6652a4429d75c6fa11b14b9f69dba
metasophiea <metasophiea@gmail.com>
2017-02-27 17:26:11 +0000

    Update messageDecrypter.vhd

--- type_2/method/96_144/1.0.1/messageDecrypter.vhd
+++ type_2/method/96_144/1.0.1/messageDecrypter.vhd
@@ -2,23 +2,23 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
-entity messageEncrypter is
+entity messageDecrypter is
     port(
         instance: in std_logic_vector(7 downto 0);
         keyIn: in std_logic_vector(((144/3) -1) downto 0);
         messageIn: in std_logic_vector(95 downto 0);
         messageOut: out std_logic_vector(95 downto 0)
     );
-end messageEncrypter;
+end messageDecrypter;
 
-architecture behaviour of messageEncrypter is
+architecture behaviour of messageDecrypter is
 -- //////// //////// //////// //////// //////// //////// //////// ////////
     -- declare constants
     constant messageLength: integer := 96;
-    constant keyLength: integer := 144;
     constant keySegments: integer := 3;
+    constant keyLength: integer := 144;
     constant keySegmentLength: integer := keyLength/keySegments;
-        
+    
      -- tools
     function morph_Z(Z: std_logic_vector)
     return std_logic_vector is variable Z_manipulator: std_logic_vector(61 downto 0) := Z;
@@ -38,9 +38,9 @@ architecture behaviour of messageEncrypter is
         for a in 0 to amount-1 loop  temp := temp(temp'length-2 downto 0) & temp(temp'length-1); end loop;
         return temp;
     end function;
-    
-    -- main encryptor work function
-    function encryptor_workFunction(messageIn, keyIn: std_logic_vector)
+
+    -- main decryptor work function
+    function decryptor_workFunction(messageIn, keyIn: std_logic_vector)
     return std_logic_vector is
         variable combiner: std_logic_vector((messageLength-1) downto 0) := (others => '0');
         variable x, y, holder, temp, key: std_logic_vector((keySegmentLength-1) downto 0) := (others => '0');
@@ -48,33 +48,33 @@ architecture behaviour of messageEncrypter is
         -- aquire parts
         x(keySegmentLength-1 downto 0) := messageIn((2*keySegmentLength)-1 downto keySegmentLength);
         y(keySegmentLength-1 downto 0) := messageIn(keySegmentLength-1 downto 0);
-        key := keyIn;
+        key(keySegmentLength-1 downto 0) := keyIn(keyLength-1-(keySegmentLength*(keySegments-1)) downto keyLength-keySegmentLength-(keySegmentLength*(keySegments-1)));
     
-        -- encryption
-        holder := x;
-        temp(keySegmentLength-1 downto 0) := leftRotate(x(keySegmentLength-1 downto 0),1) and leftRotate(x(keySegmentLength-1 downto 0),8);
-        temp(keySegmentLength-1 downto 0) := temp(keySegmentLength-1 downto 0) xor y(keySegmentLength-1 downto 0);
-        temp(keySegmentLength-1 downto 0) := temp(keySegmentLength-1 downto 0) xor leftRotate(x(keySegmentLength-1 downto 0),2);
+        -- decryption
+        holder := y;
+        temp(keySegmentLength-1 downto 0) := leftRotate(y(keySegmentLength-1 downto 0),1) and leftRotate(y(keySegmentLength-1 downto 0),8);
+        temp(keySegmentLength-1 downto 0) := temp(keySegmentLength-1 downto 0) xor x(keySegmentLength-1 downto 0);
+        temp(keySegmentLength-1 downto 0) := temp(keySegmentLength-1 downto 0) xor leftRotate(y(keySegmentLength-1 downto 0),2);
         temp(keySegmentLength-1 downto 0) := temp(keySegmentLength-1 downto 0) xor key(keySegmentLength-1 downto 0);
-        x := temp; y := holder;
+        y := temp; x := holder;  
     
-        -- recombination
+         -- recombination
         combiner((2*keySegmentLength)-1 downto keySegmentLength) := x(keySegmentLength-1 downto 0);
         combiner(keySegmentLength-1 downto 0) := y(keySegmentLength-1 downto 0); 
         return combiner;
     end function; 
 
-    -- main encryptor
-    function encrypt(messageIn, keyIn: std_logic_vector)
+    -- main decryptor
+    function decrypt(messageIn, keyIn: std_logic_vector)
     return std_logic_vector is
     begin
-        return encryptor_workFunction(messageIn, keyIn);
+        return decryptor_workFunction(messageIn, keyIn);
     end function;
 
 begin
 -- //////// //////// //////// //////// //////// //////// //////// ////////
-    process(messageIn, keyIn) 
-    begin 
-        messageOut <= encrypt(messageIn,keyIn);
+    process(messageIn, keyIn)
+    begin
+         messageOut <= decrypt(messageIn,keyIn);
     end process;
 end behaviour;
