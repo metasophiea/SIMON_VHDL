d1627a3d39a6b5b2e8781c9ec23471755624effa
metasophiea <metasophiea@gmail.com>
2017-02-28 16:38:56 +0000

    Add files via upload

---
+++ type_3/unified/0.1.0/messageEncrypter.vhd
@@ -0,0 +1,100 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity messageEncrypter is
+    port(
+        instance: in std_logic_vector(7 downto 0);
+        method: in std_logic_vector(3 downto 0);
+        keyIn: in std_logic_vector(255 downto 0);
+        messageIn: in std_logic_vector(127 downto 0);
+        messageOut: out std_logic_vector(127 downto 0)
+    );
+end messageEncrypter;
+
+architecture behaviour of messageEncrypter is
+-- //////// //////// //////// //////// //////// //////// //////// ////////
+     -- tools
+    function morph_Z(Z: std_logic_vector)
+    return std_logic_vector is variable Z_manipulator: std_logic_vector(61 downto 0) := Z;
+    begin return Z_manipulator(Z_manipulator'length-2 downto 0) & Z_manipulator(Z_manipulator'length-1);
+    end function;
+
+    function rightRotate(logic: std_logic_vector; amount: integer)
+    return std_logic_vector is  variable temp: std_logic_vector(logic'length-1 downto 0) := logic;
+    begin
+        for a in 0 to amount-1 loop  temp := temp(0) & temp(temp'length-1 downto 1);  end loop;
+        return temp;
+    end function;
+
+    function leftRotate(logic: std_logic_vector; amount: integer)
+    return std_logic_vector is  variable temp: std_logic_vector(logic'length-1 downto 0) := logic;
+    begin
+        for a in 0 to amount-1 loop  temp := temp(temp'length-2 downto 0) & temp(temp'length-1); end loop;
+        return temp;
+    end function;
+    
+    -- main encryptor work function
+    function encryptor_workFunction(messageIn, keyIn: std_logic_vector; segmentLength, keyLength, keySegments: integer)
+    return std_logic_vector is
+        variable combiner: std_logic_vector(127 downto 0) := (others => '0');
+        variable x, y, holder, temp, key: std_logic_vector(63 downto 0) := (others => '0');
+    begin
+        -- aquire parts
+        x(segmentLength-1 downto 0) := messageIn((2*segmentLength)-1 downto segmentLength);
+        y(segmentLength-1 downto 0) := messageIn(segmentLength-1 downto 0);
+        key(segmentLength-1 downto 0) := keyIn(keyLength-1-(segmentLength*(keySegments-1)) downto keyLength-segmentLength-(segmentLength*(keySegments-1)));
+    
+        -- encryption
+        holder := x;
+        temp(segmentLength-1 downto 0) := leftRotate(x(segmentLength-1 downto 0),1) and leftRotate(x(segmentLength-1 downto 0),8);
+        temp(segmentLength-1 downto 0) := temp(segmentLength-1 downto 0) xor y(segmentLength-1 downto 0);
+        temp(segmentLength-1 downto 0) := temp(segmentLength-1 downto 0) xor leftRotate(x(segmentLength-1 downto 0),2);
+        temp(segmentLength-1 downto 0) := temp(segmentLength-1 downto 0) xor key(segmentLength-1 downto 0);
+        x := temp; y := holder;
+    
+        -- recombination
+        combiner((2*segmentLength)-1 downto segmentLength) := x(segmentLength-1 downto 0);
+        combiner(segmentLength-1 downto 0) := y(segmentLength-1 downto 0); 
+        return combiner;
+    end function; 
+
+    -- main encryptor
+    function encrypt(messageIn, keyIn, method: std_logic_vector)
+    return std_logic_vector is
+        variable segmentLength, keyLength, keySegments: integer := 0;
+    begin
+        case (std_logic_vector(method)) is
+            when "0000" => segmentLength := 16; keySegments := 4; keyLength := 64;  return encryptor_workFunction(messageIn, keyIn, segmentLength, keyLength, keySegments);
+            when "0001" => segmentLength := 24; keySegments := 3; keyLength := 72;  return encryptor_workFunction(messageIn, keyIn, segmentLength, keyLength, keySegments);
+            when "0010" => segmentLength := 24; keySegments := 4; keyLength := 96;  return encryptor_workFunction(messageIn, keyIn, segmentLength, keyLength, keySegments);
+            when "0011" => segmentLength := 32; keySegments := 3; keyLength := 96;  return encryptor_workFunction(messageIn, keyIn, segmentLength, keyLength, keySegments);
+            when "0100" => segmentLength := 32; keySegments := 4; keyLength := 128; return encryptor_workFunction(messageIn, keyIn, segmentLength, keyLength, keySegments);
+            when "0101" => segmentLength := 48; keySegments := 2; keyLength := 96;  return encryptor_workFunction(messageIn, keyIn, segmentLength, keyLength, keySegments);
+            when "0110" => segmentLength := 48; keySegments := 3; keyLength := 144; return encryptor_workFunction(messageIn, keyIn, segmentLength, keyLength, keySegments);
+            when "0111" => segmentLength := 64; keySegments := 2; keyLength := 128; return encryptor_workFunction(messageIn, keyIn, segmentLength, keyLength, keySegments);
+            when "1000" => segmentLength := 64; keySegments := 3; keyLength := 192; return encryptor_workFunction(messageIn, keyIn, segmentLength, keyLength, keySegments);
+            when "1001" => segmentLength := 64; keySegments := 4; keyLength := 256; return encryptor_workFunction(messageIn, keyIn, segmentLength, keyLength, keySegments);
+            when others => segmentLength := 64; keySegments := 4; keyLength := 256; return encryptor_workFunction(messageIn, keyIn, segmentLength, keyLength, keySegments);
+        end case; 
+    end function;
+
+begin
+-- //////// //////// //////// //////// //////// //////// //////// ////////
+    process(messageIn, keyIn, method) 
+    begin 
+        case method is
+            when "0000" => if to_integer(unsigned(instance)) > (32-1) then messageOut <= messageIn; else messageOut <= encrypt(messageIn,keyIn,method); end if;
+            when "0001" => if to_integer(unsigned(instance)) > (36-1) then messageOut <= messageIn; else messageOut <= encrypt(messageIn,keyIn,method); end if;
+            when "0010" => if to_integer(unsigned(instance)) > (36-1) then messageOut <= messageIn; else messageOut <= encrypt(messageIn,keyIn,method); end if;
+            when "0011" => if to_integer(unsigned(instance)) > (42-1) then messageOut <= messageIn; else messageOut <= encrypt(messageIn,keyIn,method); end if;
+            when "0100" => if to_integer(unsigned(instance)) > (44-1) then messageOut <= messageIn; else messageOut <= encrypt(messageIn,keyIn,method); end if;
+            when "0101" => if to_integer(unsigned(instance)) > (52-1) then messageOut <= messageIn; else messageOut <= encrypt(messageIn,keyIn,method); end if;
+            when "0110" => if to_integer(unsigned(instance)) > (54-1) then messageOut <= messageIn; else messageOut <= encrypt(messageIn,keyIn,method); end if;
+            when "0111" => if to_integer(unsigned(instance)) > (68-1) then messageOut <= messageIn; else messageOut <= encrypt(messageIn,keyIn,method); end if;
+            when "1000" => if to_integer(unsigned(instance)) > (69-1) then messageOut <= messageIn; else messageOut <= encrypt(messageIn,keyIn,method); end if;
+            when "1001" => messageOut <= encrypt(messageIn,keyIn,method);
+            when others =>  messageOut <= messageIn;
+        end case;
+    end process;
+end behaviour;
---
+++ type_3/unified/0.1.0/simon.vhd
@@ -0,0 +1,140 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+use std.textio.all; 
+use work.printerlib.all;
+
+-- Methods | messageLength/keyLength | keySegmentLength | keySegments | messageSegments | messageSegmentLength | cryptLoopCount | Zselect
+-- 0000    | 32/64                   | 16               | 4           | 2               | 16                   | 32             | 0
+-- 0001    | 48/72                   | 24               | 3           | 2               | 24                   | 36             | 0
+-- 0010    | 48/96                   | 24               | 4           | 2               | 24                   | 36             | 1
+-- 0011    | 64/96                   | 32               | 3           | 2               | 32                   | 42             | 2
+-- 0100    | 64/128                  | 32               | 4           | 2               | 32                   | 44             | 3
+-- 0101    | 96/96                   | 48               | 2           | 2               | 48                   | 52             | 2
+-- 0110    | 96/144                  | 48               | 3           | 2               | 48                   | 54             | 3
+-- 0111    | 128/128                 | 64               | 2           | 2               | 64                   | 68             | 2
+-- 1000    | 128/192                 | 64               | 3           | 2               | 64                   | 69             | 3
+-- 1001    | 128/256                 | 64               | 4           | 2               | 64                   | 72             | 4
+
+entity simon is
+    port(
+        mode, clock, load: in std_logic;
+        method: in std_logic_vector(3 downto 0);
+        keyIn: in std_logic_vector(255 downto 0);
+        messageIn: in std_logic_vector(127 downto 0);
+        messageOut: out std_logic_vector(127 downto 0) := (others => '0')
+    );
+end simon;
+
+architecture behaviour of simon is
+-- //////// //////// //////// //////// //////// //////// //////// ////////
+    -- declare constants
+        constant messageLength: integer := 128;
+        constant keyLength: integer := 256;
+        constant maxCryptLoopCount: integer := 72;
+    
+    -- declare Z's
+        signal Z_0: std_logic_vector(61 downto 0) := "11111010001001010110000111001101111101000100101011000011100110";
+        signal Z_1: std_logic_vector(61 downto 0) := "10001110111110010011000010110101000111011111001001100001011010";
+        signal Z_2: std_logic_vector(61 downto 0) := "10101111011100000011010010011000101000010001111110010110110011";
+        signal Z_3: std_logic_vector(61 downto 0) := "11011011101011000110010111100000010010001010011100110100001111";
+        signal Z_4: std_logic_vector(61 downto 0) := "11010001111001101011011000100000010111000011001010010011101111";
+    
+    -- declare components
+        component completeKeyExpander
+            port(
+                method: in std_logic_vector(3 downto 0);
+                Z: in std_logic_vector(61 downto 0);
+                keyIn: in std_logic_vector((keyLength-1) downto 0);
+                keysOut: out std_logic_vector((keyLength*maxCryptLoopCount-1) downto 0)
+            );
+        end component;
+        
+        component messageEncrypter is
+            port(
+                instance: in std_logic_vector(7 downto 0);
+                method: in std_logic_vector(3 downto 0);
+                keyIn: in std_logic_vector((keyLength-1) downto 0);
+                messageIn: in std_logic_vector((messageLength-1) downto 0);
+                messageOut: out std_logic_vector((messageLength-1) downto 0)
+            );
+        end component;
+    
+    -- internal signals
+        signal internal_mode: std_logic := '0';
+        signal internal_method: std_logic_vector(3 downto 0) := (others => '0');
+        signal internal_key: std_logic_vector(keyLength-1 downto 0) := (others => '0');
+        signal internal_messageIn, internal_messageOut: std_logic_vector(messageLength-1 downto 0) := (others => '0');
+        
+        signal completeKeys: std_logic_vector((keyLength*maxCryptLoopCount-1) downto 0) := (others => '0');
+        signal useKey: std_logic_vector((keyLength-1) downto 0) := (others => '0');
+        signal selected_Z: std_logic_vector(61 downto 0);
+
+        shared variable processStage: integer := 0;
+        shared variable processLimit: integer := 72;
+
+begin
+
+    -- select Z baised on selected method
+        selected_Z <= Z_0 when (internal_method = "0000" or internal_method = "0001") else
+                      Z_1 when (internal_method = "0010") else
+                      Z_2 when (internal_method = "0011" or internal_method = "0101" or internal_method = "0111") else
+                      Z_3 when (internal_method = "0100" or internal_method = "0110" or internal_method = "1000") else
+                      Z_4; 
+
+	-- generate and connect components
+        completekeyExpansion: completeKeyExpander port map(
+            method => internal_method,
+            Z => selected_Z,
+            keyIn => internal_key,
+            keysOut => completeKeys
+        ); 
+        
+        encryptMessage: messageEncrypter port map(
+            instance => std_logic_vector(to_unsigned(0,8)),
+            method => internal_method,  
+            keyIn => useKey,  
+            messageIn => internal_messageIn,
+            messageOut => internal_messageOut
+        );
+
+    process( clock ) begin
+        if(rising_edge(clock))then
+            if(load = '1')then
+                internal_mode <= mode;
+                internal_method <= method;
+                internal_key <= keyIn;
+                internal_messageIn <= messageIn;
+                                
+                processStage := 1;
+
+                case method is
+                    when "0000" => processLimit := 32;
+                    when "0001" => processLimit := 36;
+                    when "0010" => processLimit := 36;
+                    when "0011" => processLimit := 42;
+                    when "0100" => processLimit := 44;
+                    when "0101" => processLimit := 52;
+                    when "0110" => processLimit := 54;
+                    when "0111" => processLimit := 68;
+                    when "1000" => processLimit := 69;
+                    when "1001" => processLimit := 72;
+                    when others => processLimit := 72;
+                end case;
+                
+                useKey <= keyIn;
+                
+            else
+                if(processStage = processLimit)then
+                    messageOut <= internal_messageOut;
+                else                 
+                    useKey <= completeKeys( (processStage*keyLength + (keyLength-1)) downto (processStage*keyLength) );
+                    internal_messageIn <= internal_messageOut;
+                    
+                    processStage := processStage + 1;                 
+                end if;
+            end if;
+        end if;
+    end process;
+
+end behaviour;
