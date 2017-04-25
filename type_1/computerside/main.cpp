486b1e5982597bf365a6d5c1662a2e8d0de4e624
metasophiea <metasophiea@gmail.com>
2017-04-25 21:03:34 +0100

    connectivity instructions

--- type_1/computerside/main.cpp
+++ type_1/computerside/main.cpp
@@ -74,8 +74,8 @@ int main(){
 
 //test inputs
 	//32_64  
-		bool mode = false; unsigned int method = 0; std::string key = "1918111009080100"; std::string message = "65656877";
-		//bool mode = true; unsigned int method = 0; std::string key = "1918111009080100"; std::string message = "c69be9bb";
+		//bool mode = false; unsigned int method = 0; std::string key = "1918111009080100"; std::string message = "65656877";
+		bool mode = true; unsigned int method = 0; std::string key = "1918111009080100"; std::string message = "c69be9bb";
 	//48_72  
 		//bool mode = false; unsigned int method = 1; std::string key = "1211100a0908020100"; std::string message = "6120676e696c";
 		//bool mode = true; unsigned int method = 1; std::string key = "1211100a0908020100"; std::string message = "dae5ac292cac";
